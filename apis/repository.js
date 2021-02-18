e82657dbcd8a15bf2662a3f158e2c7ec9dcd91c7
Iroha71 <y.koba.h1071@outlook.jp>
2021-02-18 18:40:03 +0900

    [fix] 認証が通らない問題を修正 #4

--- apis/repository.js
+++ apis/repository.js
@@ -1,21 +1,25 @@
 export default $axios => resource => ({
   index() {
-    return $axios.get(`${ resource }`)
+    return $axios.get(process.env.BASE_URL + resource)
+  },
+
+  index(param) {
+    return $axios.get(process.env.BASE_URL + resource, { param: param })
   },
 
   create(param) {
-    return $axios.post(`${ resource }`, param)
+    return $axios.post(process.env.BASE_URL + resource, { [resource]: param })
   },
 
   show(id) {
-    return $axios.get(`${ resource }/${ param }`)
+    return $axios.get(`${ process.env.BASE_URL + resource }/${ id }`)
   },
 
   update(id, param) {
-    return $axios.put(`${ resource }/${ id }`, param)
+    return $axios.put(`${ process.env.BASE_URL + resource }/${ id }`, { [resource]: param })
   },
 
   delete(id) {
-    return $axios.delete(`${ resource }/${ id }`)
+    return $axios.delete(`${ process.env.BASE_URL + resource }/${ id }`)
   }
 })
--- plugins/api.js
+++ plugins/api.js
@@ -5,8 +5,9 @@ export default(app, inject) => {
   const API = buildApi(app.$axios)
   const AUTH_API = buildAuthApi(app.$axios)
   const apis = {
-    user: AUTH_API(process.env.BASE_URL + 'auth'),
-    kanban: API(process.env.BASE_URL + 'kanban')
+    user: AUTH_API('auth'),
+    kanban: API('kanban'),
+    category: API('category'),
   }
 
   inject('api', apis)
